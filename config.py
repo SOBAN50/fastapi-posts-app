596e52c57467f7434117236bdbaa26173480aadc
Soban <sobanasif50@gmail.com>
2023-09-24 17:58:05 +0500

    change

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Settings(BaseSettings):
     database_username: str
     database_password: str
     database_hostname: str
+    database_port: int
     database_name: str
     secret_key: str
     access_token_expire_minutes: int
