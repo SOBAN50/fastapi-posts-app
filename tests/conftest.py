a19e8dd33a8f2a7ce8990e1002bd97d197049bf9
SOBAN ASIF <53044938+SOBAN50@users.noreply.github.com>
2025-10-29 11:41:28 +0500

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from config import Settings
+
+@pytest.fixture(autouse=True)
+def patch_settings(monkeypatch):
+    monkeypatch.setenv("database_username", "postgres")
+    monkeypatch.setenv("database_password", "postgres")
+    monkeypatch.setenv("database_hostname", "localhost")
+    monkeypatch.setenv("database_port", "5432")
+    monkeypatch.setenv("database_name", "test_db")
+    monkeypatch.setenv("secret_key", "testkey")
+    monkeypatch.setenv("algorithm", "HS256")
+    monkeypatch.setenv("access_token_expire_minutes", "30")
