eab34bde7c7dcae7cbf15d365dce8be138a198ae
SOBAN ASIF <53044938+SOBAN50@users.noreply.github.com>
2025-10-29 11:30:54 +0500

    Refactor test for root endpoint

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,12 @@
-# tests/test_main.py
 from httpx import AsyncClient
-from app.main import app
+from main import app
 import pytest
 
 @pytest.mark.asyncio
-async def test_root_endpoint():
+async def test_root():
+    """
+    Basic test for the root endpoint.
+    """
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
